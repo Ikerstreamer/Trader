43f4dce4d684fa4e735e39bad0f7670a0fdb1de6
Ikerstreamer <ikerdeaguirre@hotmail.com>
2018-02-03 16:58:45 -0800

    Made some upgrades

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var player = {
+    version:0.001,
     Offers: [{
         item: "Dirt",
         type: "Buy",
@@ -12,12 +13,73 @@ var player = {
         amnt: 300,
         timer: 200000,
     }],
+    price: {min:1,max:2},
     Tax: { Buy: 0.1, Sell: 0.1 },
     Cash: 25,
     Items: {},
+    Upgrades: [{
+        name: "Increase max price",
+        cost: 10,
+        scaling: 0.5,
+        effect: 0.1,
+        variable: function () { return player.price.max },
+        action: function () {
+            if (player.Cash >= this.cost) {
+                player.Cash -= this.cost;
+                player.price.max += this.effect;
+                this.cost *= this.scaling;
+                UpdateTable();
+            }
+        },
+    },
+    {
+        name: "Increase min price",
+        cost: 10,
+        scaling: 0.5,
+        effect: 0.1,
+        variable: function () { return player.price.min },
+        action: function () {
+            if (player.Cash >= this.cost) {
+                player.Cash -= this.cost;
+                player.price.min += this.effect;
+                this.cost *= this.scaling;
+                UpdateTable();
+            }
+        },
+    }],
     NewOfferTimer: 100000,
 }
 
+var allUpgrades = [{
+    name: "Increase max price",
+    cost: 10,
+    scaling: 0.5,
+    effect: 0.1,
+    variable: function () { return player.price.max },
+    action: function () {
+        if (player.Cash >= this.cost) {
+            player.Cash -= this.cost;
+            player.price.max += this.effect;
+            this.cost *= this.scaling;
+            UpdateTable();
+        }
+    },
+},
+    {
+    name: "Increase min price",
+    cost: 10,
+    scaling: 0.5,
+    effect: 0.1,
+    variable: function () { return player.price.min },
+    action: function () {
+        if (player.Cash >= this.cost) {
+            player.Cash -= this.cost;
+            player.price.min += this.effect;
+            this.cost *= this.scaling;
+            UpdateTable();
+        }
+    },
+} ];
 
 function selectOffer(index) {
     var Offer = player.Offers[index];
@@ -39,7 +101,7 @@ function selectOffer(index) {
         } else alert("You dont have enough cash!");
     } else if (player.Items[Offer.item] >= num) {
         var TaxMulti = 1 - player.Tax[Offer.type];
-        if (confirm("Are you sure u want to sell " + num + " units of " + Offer.item.toLowerCase() + " for $" + (num * Offer.price).toFixed(2) + " with a sales tax of "+player.Tax[Offer.type]+"% ($" + (num * Offer.price * player.Tax[Offer.type]).toFixed(2) + ")? (This would make your net profit " + (num * Offer.price * TaxMulti).toFixed(2) + "$)")) {
+        if (confirm("Are you sure u want to sell " + num + " units of " + Offer.item.toLowerCase() + " for $" + (num * Offer.price).toFixed(2) + " with a sales tax of "+player.Tax[Offer.type]+"% ($" + (num * Offer.price * player.Tax[Offer.type]).toFixed(2) + ")? (This would make your net profit $" + (num * Offer.price * TaxMulti).toFixed(2) + ")")) {
             player.Cash += num * Offer.price * TaxMulti;
             player.Items[Offer.item] -= num;
             Offer.amnt -= num;
@@ -48,18 +110,15 @@ function selectOffer(index) {
     UpdateTable();
 }
 
+function buyUpgrade(index) {player.Upgrades[index].action()}
 
 function UpdateTable() {
     var Table = document.getElementById("statsTable");
     Table.rows[0].cells[1].innerHTML = player.Cash.toFixed(2) + "$";
     var Table = document.getElementById("offersTable");
+    player.Offers = player.Offers.filter((offer) => offer.amnt > 0 && offer.timer > 0)
     for (var i = 0; i < player.Offers.length; i++) {
         var Offer = player.Offers[i];
-        if (Offer.amnt <= 0 || Offer.timer <= 0) {
-            player.Offers.splice(i, 1);
-            Table.deleteRow(i);
-            continue;
-        }
         if (Table.rows.length <= i) {
             var Row = Table.insertRow(i);
             for (var j = 0; j < 5; j++) Row.insertCell(j);
@@ -68,7 +127,9 @@ function UpdateTable() {
         for (var j = 0; j < 4; j++) Row.cells[j].innerHTML = Object.values(Offer)[j];
         Row.cells[4].innerHTML = '<button style="width:100%;" onclick="selectOffer(' + i + ')">Select</button>';
     }
+    for (var i = Table.rows.length; i > player.Offers.length; i--) Table.deleteRow(i-1);
     var Table = document.getElementById("itemsTable");
+    if (Object.keys(player.Items).length > 0) Table.parentNode.parentNode.classList.remove("hidden");
     for (var i = 0; i < Object.keys(player.Items).length; i++) {
         if (Table.rows.length <= i) {
             var Row = Table.insertRow(i);
@@ -78,12 +139,27 @@ function UpdateTable() {
         Row.cells[0].innerHTML = Object.keys(player.Items)[i];
         Row.cells[1].innerHTML = Object.values(player.Items)[i];
     }
+    var Table = document.getElementById("upgradesTable");
+    if (player.Upgrades.length > 0) Table.parentNode.parentNode.classList.remove("hidden");
+    for (var i = 0; i < player.Upgrades.length; i++) {
+        var Upgrade = player.Upgrades[i];
+        if (Table.rows.length <= i) {
+            var Row = Table.insertRow(i);
+            for (var j = 0; j < 5; j++) Row.insertCell(j);
+        }
+        var Row = Table.rows[i]
+        Row.cells[0].innerHTML = Upgrade.name;
+        Row.cells[1].innerHTML = Math.round(Upgrade.variable() * 100) + "%";
+        Row.cells[2].innerHTML = Upgrade.effect * 100 + "%";
+        Row.cells[3].innerHTML = "$" + Upgrade.cost.toFixed(2);
+        Row.cells[4].innerHTML = '<button style="width:100%;" onclick="buyUpgrade(' + i + ')">Buy</button>';
+    }
 }
 
 function init() {
-    if(localStorage.playerSave != undefined) player = JSON.parse(atob(localStorage.playerSave));
+    window.resetPlayer = player;
+    if (localStorage.playerSave != undefined || localStorage.playerSave != null) player = JSON.parse(atob(localStorage.playerSave));
     UpdateTable();
-    setInterval(NewOffers, 50);
     window.start = Date.now();
     window.clock = 0;
     window.frameTime = 50;
@@ -110,7 +186,7 @@ function init() {
 function NewOffers() {
     if (player.NewOfferTimer > 1) var rand = player.NewOfferTimer / (Math.random() * Math.pow(player.NewOfferTimer, 1.55));
     else var rand = 1;
-    if (rand >= 1) {
+    if (rand >= 0.25*player.Offers.length) {
         CreateOffer();
         player.NewOfferTimer += 60000;
         console.log("NewOffer")
@@ -118,7 +194,7 @@ function NewOffers() {
     player.NewOfferTimer -= frameTime;
     for (var i = 0; i < player.Offers.length; i++) {
         player.Offers[i].timer -= frameTime;
-        if (player.Offers[i] <= 0) UpdateTable();
+        if (player.Offers[i].timer <= 0) UpdateTable();
     }
     
 }
@@ -129,37 +205,19 @@ function CreateOffer() {
         type: "",
         price: 0,
         amnt: 0,
-        timer: 100000,
+        timer: 800000,
     }
     temp.item = "Dirt";
     if (Math.round(Math.random()) === 0) temp.type = "Buy";
     else temp.type = "Sell";
-    temp.price = parseFloat((0.1 + Math.random() * 0.05).toFixed(2));
+    temp.price = parseFloat(Math.max(Math.min(Math.random() * 0.1 * player.price.max * 2, 0.1 * player.price.max), 0.1 * player.price.min).toFixed(2));
     temp.amnt = Math.max(Math.round(500 * Math.random()), 100);
     player.Offers.push(temp)
     UpdateTable()
 }
 
 function reset() {
-    player = {
-        Offers: [{
-            item: "Dirt",
-            type: "Sell",
-            price: 0.15,
-            amnt: 100,
-            timer: 100000,
-        }, {
-            item: "Dirt",
-            type: "Buy",
-            price: 0.1,
-            amnt: 100,
-            timer: 100000,
-        }],
-        Tax: { Buy: 0.1, Sell: 0.1 },
-        Cash: 10,
-        Items: {},
-        NewOfferTimer: 100000,
-    }
+    player = resetPlayer;
     UpdateTable();
 }
 
